7df48122af4877e3f428dac06ad6e03949284d8c
Adron Hall <adron@thrashingcode.com>
2021-09-29 11:40:28 -0700

    Removed yarnlock/dependencies from root and placed in project directory.

--- ampification-first-recon/src/main.js
+++ ampification-first-recon/src/main.js
@@ -1,4 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import Amplify from 'aws-amplify'
+import awsconfig from './aws-exports'
+import {
+    applyPolyfilles,
+    applyPolyfills,
+    defineCustomElements,
+} from '@aws-amplify/ui-components/loader'
+
+applyPolyfills().then(() => {
+    defineCustomElements(window)
+})
+
+Amplify.configure(awsconfig)
 createApp(App).mount('#app')
