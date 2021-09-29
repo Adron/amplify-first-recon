004128cb99fcc112f3d45c4dd041ba864f4dd9ca
Adron Hall <adron@thrashingcode.com>
2021-09-29 12:20:52 -0700

    All these bits added.

--- ampification-first-recon/src/main.js
+++ ampification-first-recon/src/main.js
@@ -4,7 +4,6 @@ import App from './App.vue'
 import Amplify from 'aws-amplify'
 import awsconfig from './aws-exports'
 import {
-    applyPolyfilles,
     applyPolyfills,
     defineCustomElements,
 } from '@aws-amplify/ui-components/loader'
